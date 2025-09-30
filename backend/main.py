312ee806f87e4fbb72a7e7469dae2337b98961bf
Roul-max <rohitkumarrrx@gmail.com>
2025-10-01 02:14:30 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://speech-orcin-ten.vercel.app"
+        "https://speech-beryl-theta.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
