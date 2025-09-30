d8f5d20c94fc9e83af6578d4afc2a3f342d037ae
Roul-max <rohitkumarrrx@gmail.com>
2025-10-01 00:27:54 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Speech Segmentation API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://speech-orcin-ten.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
